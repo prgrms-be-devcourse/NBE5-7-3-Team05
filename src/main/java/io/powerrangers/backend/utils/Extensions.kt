1b95e7275dff9e9d6074c24683b9fcc334dbb068
msmsm0320 <ms000320@naver.com>
2025-06-11 16:48:41 +0900

    Refactor : User DTO 마이그레이션

---
+++ src/main/java/io/powerrangers/backend/utils/Extensions.kt
@@ -0,0 +1,23 @@
+package io.powerrangers.backend.utils
+
+import io.powerrangers.backend.dto.UserFollowResponseDto
+import io.powerrangers.backend.dto.UserGetProfileResponseDto
+import io.powerrangers.backend.entity.User
+
+fun User.toProfileResponseDto() : UserGetProfileResponseDto {
+    return UserGetProfileResponseDto(
+        userId = this.id!!,
+        nickname = this.nickname,
+        intro = this.intro,
+        profileImage = this.profileImage
+    )
+}
+
+fun User.toFollowResponseDto(): UserFollowResponseDto {
+    return UserFollowResponseDto(
+        id = this.id!!,
+        nickname = this.nickname,
+        intro = this.intro,
+        profileImage = this.profileImage
+    )
+}
