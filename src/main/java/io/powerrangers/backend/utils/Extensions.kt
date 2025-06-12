6abceba18e2a57d1c408712d4299a7a55f63ebba
Taeho KIM <laferrari963@naver.com>
2025-06-12 11:46:44 +0900

    Refactor : comment 도메인 2차 마이그레이션

--- src/main/java/io/powerrangers/backend/utils/Extensions.kt
+++ src/main/java/io/powerrangers/backend/utils/Extensions.kt
@@ -3,6 +3,9 @@ package io.powerrangers.backend.utils
 import io.powerrangers.backend.dto.UserDetails
 import io.powerrangers.backend.dto.UserFollowResponseDto
 import io.powerrangers.backend.dto.UserGetProfileResponseDto
+import io.powerrangers.backend.dto.comment.CommentResponseDto
+import io.powerrangers.backend.dto.comment.CommentUpdateResponseDto
+import io.powerrangers.backend.entity.Comment
 import io.powerrangers.backend.entity.User
 
 fun User.toProfileResponseDto(): UserGetProfileResponseDto {
@@ -33,3 +36,26 @@ fun User.toUserDetails(user: User): UserDetails {
         profileImage = user.profileImage
     )
 }
+
+fun Comment.toResponseDto(children: List<CommentResponseDto> = emptyList()): CommentResponseDto {
+    return CommentResponseDto(
+        id = this.id!!,
+        content = this.content,
+        nickname = this.user.nickname,
+        profileImage = this.user.profileImage,
+        createdAt = this.createdAt,
+        userId = this.user.id!!,
+        children = children
+    )
+}
+
+fun Comment.toUpdateResponseDto(): CommentUpdateResponseDto{
+    return CommentUpdateResponseDto(
+        id=this.id!!,
+        content = this.content,
+        nickname = this.user.nickname,
+        profileImage = this.user.profileImage
+    )
+}
+
+
