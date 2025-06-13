51c007b64973b419568d4edcb8a1bf06186ae4d0
jwh946 <garcon0406@gmail.com>
2025-06-13 14:16:40 +0900

    Refactor : TaskRepository 테스트가 H2 DB로 동작하도록 변경

--- src/test/java/io/powerrangers/backend/dao/TaskRepositoryTests.kt
+++ src/test/java/io/powerrangers/backend/dao/TaskRepositoryTests.kt
@@ -8,12 +8,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import java.time.LocalDateTime
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TaskRepositoryTests @Autowired constructor(
     val taskRepository: TaskRepository,
     val userRepository: UserRepository
