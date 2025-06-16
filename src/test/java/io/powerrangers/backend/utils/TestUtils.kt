3da3582ced0272e062b470923914f7aea679868f
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-06-16 10:39:48 +0900

    Test : FollowService 유닛 테스트 완료

--- src/test/java/io/powerrangers/backend/utils/TestUtils.kt
+++ src/test/java/io/powerrangers/backend/utils/TestUtils.kt
@@ -2,6 +2,17 @@ package io.powerrangers.backend.utils
 
 import io.powerrangers.backend.entity.User
 
+fun genUserWithId(id: Long, nickname: String, email:String) =
+    User(
+        id = id,
+        nickname = nickname,
+        email = email,
+        provider = "Test",
+        providerId = "TestProviderId",
+        followers = mutableListOf(),
+        followings = mutableListOf()
+    )
+
 fun genUser(nickname: String, email:String) =
     User(
         nickname = nickname,
@@ -12,6 +23,14 @@ fun genUser(nickname: String, email:String) =
         followings = mutableListOf()
     )
 
+fun genUserListWithId(size: Int) : List<User> {
+    val users = mutableListOf<User>()
+    for(i in 0 until size) {
+        users.add(genUserWithId(id = 10L + i, nickname="user_$i", email = "user_${i}@email.com"))
+    }
+    return users
+}
+
 fun genUserList(size: Int) : List<User> {
     val users = mutableListOf<User>()
     for(i in 0 until size) {
@@ -19,3 +38,4 @@ fun genUserList(size: Int) : List<User> {
     }
     return users
 }
+
