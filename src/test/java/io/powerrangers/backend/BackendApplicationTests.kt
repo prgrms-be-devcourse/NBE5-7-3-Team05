26c8b7789a31284f4a4a00c5a9183807ba118e5e
Taeho KIM <laferrari963@naver.com>
2025-06-17 15:49:02 +0900

    Update BackendApplicationTests.kt

--- src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
+++ src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
@@ -1,10 +1,19 @@
 package io.powerrangers.backend
 
+import io.powerrangers.backend.dao.adapter.RedisSubscriber
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
 
 @SpringBootTest
 internal class BackendApplicationTests {
+
+    @Mock
+    lateinit var redisMessageListenerContainer: RedisMessageListenerContainer
+
+    @Mock
+    lateinit var redisSubscriber: RedisSubscriber
     @Test
     fun contextLoads() {
     }
