09857abfcc2b5cd44b34d9e77e48d593fc778a83
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-06-17 09:27:33 +0900

    Feat: CI 빌드/테스트 자동화 + Slack 알림 기능
    * Feat: CI 빌드/테스트 자동화
    * Fix : 빌드 안되는 문제, 테스트용 DB 등록, 테스트 설정 파일 만들기
    * Fix : ActiveProfiles 어노테이션 추가
    * Feat : Slack에 빌드, 테스트 성공 및 실패 시 알림

--- src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
+++ src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
@@ -2,8 +2,10 @@ package io.powerrangers.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 internal class BackendApplicationTests {
     @Test
     fun contextLoads() {
