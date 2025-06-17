c0875df402e46cc51bc573ac77c4fb9d1b2c389f
Taeho KIM <laferrari963@naver.com>
2025-06-17 16:11:21 +0900

    Refactor: BackendApplicationTests.kt

--- src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
+++ src/test/java/io/powerrangers/backend/BackendApplicationTests.kt
@@ -1,10 +1,19 @@
 package io.powerrangers.backend
 
+import io.powerrangers.backend.dao.adapter.RedisSubscriber
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
 internal class BackendApplicationTests {
+    @MockitoBean
+    lateinit var redisMessageListenerContainer: RedisMessageListenerContainer
+
+    @MockitoBean
+    lateinit var redisSubscriber: RedisSubscriber
+
     @Test
     fun contextLoads() {
     }
