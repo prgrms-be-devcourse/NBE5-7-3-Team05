965056414c98de89a40f2d10fac29327d07333b0
sleepyhoon <pokjm99@naver.com>
2025-06-17 11:04:13 +0900

    Feat: createdAt 필드 추가

--- src/main/java/io/powerrangers/backend/entity/RefreshToken.kt
+++ src/main/java/io/powerrangers/backend/entity/RefreshToken.kt
@@ -1,8 +1,16 @@
 package io.powerrangers.backend.entity
 
 import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
 @Entity
+@Table(
+    name = "refresh_token",
+    indexes = [Index(name = "idx_refresh_token_created_at", columnList = "createdAt")]
+)
+@EntityListeners(AuditingEntityListener::class)
 class RefreshToken (
     @Id
     @Column(name = "refresh_token_id")
@@ -13,5 +21,9 @@ class RefreshToken (
     @JoinColumn(name = "user_id", nullable = true)
     var user: User?,
 
-    val refreshToken: String
+    val refreshToken: String,
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    val createdAt: LocalDateTime
 )
